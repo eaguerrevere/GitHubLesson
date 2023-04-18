a1d6665fc490acf6dca510636b9f8423a7ec78ce
Eli <elizabeth.aguerrevere@gmail.com>
2023-04-18 07:55:57 -0700

    Initial Commit

---
+++ GitHubLesson/GitHubLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubLessonApp.swift
+//  GitHubLesson
+//
+//  Created by Elizabeth Aguerrevere on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
